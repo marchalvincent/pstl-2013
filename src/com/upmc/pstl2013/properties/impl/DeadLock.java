1c9c6fc0596e956d4041434c2ee4f9ec1cbad1d6
Vincent <Vincent@Vincent-PC>
2013-01-26 16:56:06 +0100

    Ajout de la PersonalPropertie

--- src/com/upmc/pstl2013/properties/impl/DeadLock.java
+++ src/com/upmc/pstl2013/properties/impl/DeadLock.java
@@ -2,16 +2,10 @@ package com.upmc.pstl2013.properties.impl;
 
 import java.util.Map;
 
-
 public class DeadLock extends AbstractProperties {
 
-	
 	public DeadLock(Map<String, String> properties) {
 		super(properties);
-	}
-	
-	@Override
-	public String getAlloyCode() {
-		return "run testAll for 0 but 20 State, 15 Object, 5 ActivityNode, 4 ActivityEdge expect 1";
+		super.alloyCode = "run testAll for 0 but 20 State, 15 Object, 5 ActivityNode, 4 ActivityEdge expect 1";
 	}
 }
