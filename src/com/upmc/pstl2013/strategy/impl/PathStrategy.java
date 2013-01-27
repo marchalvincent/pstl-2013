e9598b592a19e6c3ce03e89f2e234a2be838bead
Xantho <Xantho@Xantho-PC>
2013-01-27 22:57:51 +0100

    Modification de la view (cot? properties)

---
+++ src/com/upmc/pstl2013/strategy/impl/PathStrategy.java
@@ -0,0 +1,24 @@
+package com.upmc.pstl2013.strategy.impl;
+
+import com.upmc.pstl2013.strategy.IStrategy;
+
+import edu.mit.csail.sdg.alloy4compiler.translator.A4Solution;
+
+/**
+ * Représente une strategie de parcours qui permet de récupérer
+ * le chemin trouvé par la génération Alloy.
+ * 
+ */
+public class PathStrategy implements IStrategy {
+
+	public PathStrategy() {
+		super();
+	}
+	
+	@Override
+	public String parcours(A4Solution ans) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
