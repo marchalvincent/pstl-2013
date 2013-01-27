e72a1360057040c3f3bf1c0da1b452482c5f7f72
Vincent <Vincent@Vincent-PC>
2013-01-27 21:04:21 +0100

    Ajout du bouton DL log, qui le cr?? dans le dossier de destination

---
+++ src/com/upmc/pstl2013/views/LogCreator.java
@@ -0,0 +1,38 @@
+package com.upmc.pstl2013.views;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class LogCreator {
+
+	private static final String logPath = "log.html";
+
+	public static void createLog(String path) throws IOException {
+		File logs = new File(logPath);
+		File destination = new File(path + logPath);
+
+		FileInputStream in = null;
+		FileOutputStream out = null;
+		
+		try {
+			in = new FileInputStream(logs);
+			out = new FileOutputStream(destination);
+
+			byte buffer[] = new byte[512*1024]; 
+			int nbLecture; 
+			while( (nbLecture = in.read(buffer)) != -1 ) { 
+				out.write(buffer, 0, nbLecture); 
+			} 
+
+		} catch (IOException e) {
+			throw e;
+		} finally {
+			in.close();
+			out.close();
+		}
+		logs.delete();
+	}
+
+}
