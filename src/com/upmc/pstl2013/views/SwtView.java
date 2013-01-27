48ad0ea8104c71a88bdb43b7ac91af0e8c106e14
Xantho <Xantho@Xantho-PC>
2013-01-27 19:41:23 +0100

    MAJ de l'interface graph compl?te

--- src/com/upmc/pstl2013/views/SwtView.java
+++ src/com/upmc/pstl2013/views/SwtView.java
@@ -1,6 +1,5 @@
 package com.upmc.pstl2013.views;
 
-import java.awt.Event;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -14,12 +13,17 @@ import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.TableEditor;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
@@ -37,6 +41,7 @@ import com.upmc.pstl2013.alloyGenerator.impl.JetException;
 import com.upmc.pstl2013.factory.Factory;
 import com.upmc.pstl2013.infoGenerator.IInfoGenerator;
 import com.upmc.pstl2013.infoParser.IInfoParser;
+import com.upmc.pstl2013.properties.impl.AbstractProperties;
 import com.upmc.pstl2013.umlParser.IUMLParser;
 
 import edu.mit.csail.sdg.alloy4.Err;
@@ -63,6 +68,8 @@ public class SwtView extends Composite {
 	private Composite cpItemAlloyUse,cpdItemAlloyProp ;
 	private Table tabProperties;
 	private Table tabValueProperties;
+	private final TableEditor editor;
+	private final int EDITABLECOLUMN = 1;
 
 
 	/**
@@ -84,7 +91,8 @@ public class SwtView extends Composite {
 
 		//TabFolder
 		tabFolder = new TabFolder(this, SWT.NONE);
-
+		tabFolder.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		
 
 		itemAlloyUse = new TabItem(tabFolder, SWT.NONE);
 		itemAlloyUse.setText("Utilisation");
@@ -95,7 +103,7 @@ public class SwtView extends Composite {
 		itemAlloyProperty = new TabItem(tabFolder, SWT.NONE);
 		itemAlloyProperty.setText("Properties");
 		cpdItemAlloyProp= new Composite(tabFolder, SWT.BORDER);
-		cpdItemAlloyProp.setLayout(new GridLayout(3, false));
+		cpdItemAlloyProp.setLayout(new GridLayout(2, false));
 		itemAlloyProperty.setControl(cpdItemAlloyProp);
 		
 		tabProperties = new Table(cpdItemAlloyProp, SWT.BORDER | SWT.CHECK | SWT.FULL_SELECTION | SWT.MULTI );
@@ -105,17 +113,17 @@ public class SwtView extends Composite {
 		addProperties();
 
 		tabValueProperties = new Table(cpdItemAlloyProp,  SWT.MULTI |SWT.BORDER | SWT.FULL_SELECTION);
+		tabValueProperties.setTouchEnabled(true);
 		tabValueProperties.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		tabValueProperties.setHeaderVisible(true);
 		tabValueProperties.setLinesVisible(true);
+		editor = new TableEditor(tabValueProperties);
 		
 		String[] titlesVP = { "Key", "Value" };
 		for (int i = 0; i < titlesVP.length; i++) {
 			TableColumn column = new TableColumn(tabValueProperties, SWT.NONE);
 			column.setText(titlesVP[i]);
 		}
-		
-		new Label(cpdItemAlloyProp, SWT.NONE);
 
 
 		GridLayout gridLayout = new GridLayout(1, false);
@@ -135,7 +143,9 @@ public class SwtView extends Composite {
 				}
 			}
 		});
-		btnChooseDir.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+		GridData gd_btnChooseDir = new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1);
+		gd_btnChooseDir.widthHint = 87;
+		btnChooseDir.setLayoutData(gd_btnChooseDir);
 		btnChooseDir.setText("Choose Dir");
 
 		textDirectory = new Text(cpItemAlloyUse, SWT.BORDER);
@@ -172,7 +182,7 @@ public class SwtView extends Composite {
 		btnChooserFile.setText("Chooser File");
 
 		text = new Text(cpItemAlloyUse, SWT.BORDER | SWT.READ_ONLY | SWT.H_SCROLL | SWT.V_SCROLL | SWT.CANCEL | SWT.MULTI);
-		GridData gd_text = new GridData(SWT.FILL, SWT.TOP, true, true, 1, 6);
+		GridData gd_text = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 6);
 		gd_text.widthHint = 297;
 		gd_text.heightHint = 192;
 		text.setLayoutData(gd_text);
@@ -187,10 +197,6 @@ public class SwtView extends Composite {
 		new Label(cpItemAlloyUse, SWT.NONE);
 		new Label(cpItemAlloyUse, SWT.NONE);
 		new Label(cpItemAlloyUse, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
 
 		btnExcuterAlloy.addMouseListener(new MouseAdapter() {
 			@Override
@@ -216,8 +222,6 @@ public class SwtView extends Composite {
 				System.out.println(log.getAllAppenders().toString());
 			}
 		});
-
-
 
 		// on finit par une petite vérification...
 		this.checkDirectory(alloyGenerator);
@@ -247,24 +251,17 @@ public class SwtView extends Composite {
 
 	private void addProperties()
 	{
-		String[] titlesP = { " ", "Properties" };
-		for (int i = 0; i < titlesP.length; i++) {
-			TableColumn column = new TableColumn(tabProperties, SWT.NONE);
-			column.setText(titlesP[i]);
-		}
+		TableColumn column = new TableColumn(tabProperties, SWT.NONE);
+		column.setText( "Properties");
 
-		for (int i = 0; i < 10; i++) {
+		for (String prop : AbstractProperties.getProperties()) {
 			TableItem item = new TableItem(tabProperties, SWT.NONE);
-			item.setText(0, "" + i);
-			item.setText(1, "y");
+			item.setText(0, prop);
 		}
 
-		for (int i=0; i<titlesP.length; i++) {
-			tabProperties.getColumn (i).pack ();
-		}     
-
-		tabProperties.addListener(SWT.Selection, new Listener() {
-
+		tabProperties.getColumn(0).pack ();
+		tabProperties.addListener(SWT.Selection, new Listener() 
+		{
 			@Override
 			public void handleEvent(org.eclipse.swt.widgets.Event  e) {
 				String string = "";
@@ -280,11 +277,45 @@ public class SwtView extends Composite {
 	{
 
 		tabValueProperties.removeAll();
+		
+		tabValueProperties.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				// Clean up any previous editor control
+				Control oldEditor = editor.getEditor();
+				if (oldEditor != null)
+					oldEditor.dispose();
+
+				// Identify the selected row
+				TableItem item = (TableItem) e.item;
+				if (item == null)
+					return;
+
+				// The control that will be the editor must be a child of the
+				// Table
+				Text newEditor = new Text(tabValueProperties, SWT.NONE);
+				newEditor.setText(item.getText(EDITABLECOLUMN));
+				newEditor.addModifyListener(new ModifyListener() 
+				{
+					@Override
+					public void modifyText(ModifyEvent e) {
+						Text text = (Text) editor.getEditor();
+						editor.getItem()
+						.setText(EDITABLECOLUMN, text.getText());
+					}
+				});
+				newEditor.selectAll();
+				newEditor.setFocus();
+				editor.setEditor(newEditor, item, EDITABLECOLUMN);
+			}
+		});
+		
 		for (int i = 0; i < 10; i++) {
 			TableItem item = new TableItem(tabValueProperties, SWT.NONE);
 			item.setText(0, nameProperty);
 			item.setText(1, "y");
 		}
+		
+		
 
 		for (int i=0; i<2; i++) {
 			tabValueProperties.getColumn (i).pack ();
