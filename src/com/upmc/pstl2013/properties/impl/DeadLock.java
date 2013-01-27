282f1571a1eb4e0dbf6743ded7f62db70e5de122
Vincent <Vincent@Vincent-PC>
2013-01-27 22:57:39 +0100

    dqs

---
+++ src/com/upmc/pstl2013/properties/impl/DeadLock.java
@@ -0,0 +1,11 @@
+package com.upmc.pstl2013.properties.impl;
+
+import java.util.Map;
+
+public class DeadLock extends AbstractProperties {
+
+	public DeadLock(Map<String, String> properties) {
+		super(properties);
+		super.alloyCode = "run testAll for 0 but 20 State, 15 Object, 5 ActivityNode, 4 ActivityEdge expect 1";
+	}
+}
