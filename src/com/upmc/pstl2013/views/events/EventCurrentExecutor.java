07aed07c09a5cf5137eadc14a93b62593a06985d
Xantho <Xantho@Xantho-PC.formation.local>
2013-01-31 15:32:13 +0100

    Mise en place de l'afficahe des attributes d'une properties

--- src/com/upmc/pstl2013/views/events/EventCurrentExecutor.java
+++ src/com/upmc/pstl2013/views/events/EventCurrentExecutor.java
@@ -1,13 +1,15 @@
 package com.upmc.pstl2013.views.events;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
+
 import com.upmc.pstl2013.properties.IProperties;
 import com.upmc.pstl2013.views.SwtView;
 
 public class EventCurrentExecutor extends AbstractEventExecutor {
 
-	@SuppressWarnings("unused")
 	private Table tabProperties;
 
 	/**
@@ -23,13 +25,14 @@ public class EventCurrentExecutor extends AbstractEventExecutor {
 	@Override
 	protected List<IProperties> getProperties() {
 
-//		List<IProperties> properties = new HashMap<String, Map<String, String>>();
-//		for (TableItem item : tabProperties.getItems()) {
-//			if (item.getChecked()) {
-//				// TODO Récupéréer les clés valeurs.
-//				properties.put(item.getText(), null);
-//			}
-//		}
+		List<IProperties> properties = new ArrayList<IProperties>();
+		for (TableItem item : tabProperties.getItems()) {
+			if (item.getChecked()) {
+				// TODO Récupéréer les clés valeurs.
+				
+				//properties.put(item.getText(), null);
+			}
+		}
 		//TODO MICHEL
 		return null;
 	}
