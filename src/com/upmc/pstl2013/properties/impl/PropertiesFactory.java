ab3b5d747227a9d5d62acaee173d5eb8bf53a12b
Vincent <Vincent@Vincent-PC>
2013-02-02 13:26:44 +0100

    Modification de la factory de properties. Les propri?t?s sont maintenant en static pour n'avoir qu'une seule instance de chaque.

--- src/com/upmc/pstl2013/properties/impl/PropertiesFactory.java
+++ src/com/upmc/pstl2013/properties/impl/PropertiesFactory.java
@@ -1,5 +1,6 @@
 package com.upmc.pstl2013.properties.impl;
 
+import java.util.Map;
 import org.apache.log4j.Logger;
 import com.upmc.pstl2013.properties.IProperties;
 
@@ -10,6 +11,7 @@ import com.upmc.pstl2013.properties.IProperties;
 public class PropertiesFactory {
 
 	private static Logger log = Logger.getLogger(PropertiesFactory.class);
+	private static Map<String, IProperties> properties;
 
 	/**
 	 * Méthode static pour la création des propriétés.
@@ -20,17 +22,24 @@ public class PropertiesFactory {
 	 */
 	public static IProperties createPropertie(String name) throws PropertiesException {
 
-		// puis on créer l'objet propertie
-		if (name.equals(PersonalPropertie.class.getSimpleName())) 	return new PersonalPropertie();
-		else if (name.equals(DeadLock.class.getSimpleName())) 		return new DeadLock();
-		else if (name.equals(EnoughState.class.getSimpleName())) 	return new EnoughState();
-		else if (name.equals(Orga.class.getSimpleName())) 			return new Orga();
-		else if (name.equals(Wf.class.getSimpleName())) 			return new Wf();
-		// ici on peut ajouter les nouvelles propriétés
+		// si l'objet est déjà créé, on le renvoie
+		IProperties prop = properties.get(name);
+		if (prop != null) {
+			return prop;
+		}
+		
+		// sinon, on créer l'objet, on l'ajoute dans la map et on le renvoie
+		if (name.equals(PersonalPropertie.class.getSimpleName())) 	properties.put(name, new PersonalPropertie());
+		else if (name.equals(DeadLock.class.getSimpleName())) 		properties.put(name, new DeadLock());
+		else if (name.equals(EnoughState.class.getSimpleName())) 	properties.put(name, new EnoughState());
+		else if (name.equals(Orga.class.getSimpleName())) 			properties.put(name, new Orga());
+		else if (name.equals(Wf.class.getSimpleName())) 			properties.put(name, new Wf());
+		// c'est ici qu'on ajoute les nouvelles propriétés créées
 		else {
 			final String error = "Le nom de la propriété n'existe pas dans la factory : " + name;
 			log.warn(error);
 			throw new PropertiesException(error);
 		}
+		return properties.get(name);
 	}
 }
