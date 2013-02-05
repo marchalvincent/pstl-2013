bb6903d73bbef8e4c432176dbab7bcd48b409849
Vincent <Vincent@Vincent-PC>
2013-02-05 12:15:18 +0100

    modif strat?gie ne pas se mettre a jour la dessus

--- src/com/upmc/pstl2013/strategy/impl/MyA4Solution.java
+++ src/com/upmc/pstl2013/strategy/impl/MyA4Solution.java
@@ -1,8 +1,53 @@
 package com.upmc.pstl2013.strategy.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class MyA4Solution {
 
-	private int nbRunning;
+	public static int HELD_TOKEN = 1;
+	public static int OFFERS = 2;
 	
+	private int nbRunning;
+	private Map<Integer, List<String>> offers;
+	private Map<Integer, List<String>> heldTokens;
+	
+	public MyA4Solution() {
+		super();
+		nbRunning = 0;
+		offers = new HashMap<Integer, List<String>>();
+		heldTokens = new HashMap<Integer, List<String>>();
+	}
+	
+	public void setNbRunning(int nb) {
+		nbRunning = nb;
+	}
+	
+	public int getNbRunning() {
+		return nbRunning;
+	}
+	
+	public void addOffers(Integer nbRound, String name) {
+		if (offers.get(nbRound) == null) {
+			offers.put(nbRound, new ArrayList<String>());
+		}
+		offers.get(nbRound).add(name);
+	}
+	
+	public void addHeldTokens(Integer nbRound, String name) {
+		if (heldTokens.get(nbRound) == null) {
+			heldTokens.put(nbRound, new ArrayList<String>());
+		}
+		heldTokens.get(nbRound).add(name);
+	}
+	
+	public List<String> getRound(Integer i) {
+		List<String> liste = new ArrayList<String>();
+		liste.addAll(heldTokens.get(i));
+		liste.addAll(offers.get(i));
+		return liste;
+	}
 }
