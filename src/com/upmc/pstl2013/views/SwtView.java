e81f9050ba702a40fcce43fc460b604d633d781f
Xantho <Xantho@Xantho-PC>
2013-02-08 11:45:33 +0100

    Pr?paration au timeOut et a la selection des resultat d'execution

--- src/com/upmc/pstl2013/views/SwtView.java
+++ src/com/upmc/pstl2013/views/SwtView.java
@@ -11,7 +11,6 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
@@ -19,6 +18,8 @@ import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
 
 import com.upmc.pstl2013.alloyExecutor.IAlloyExecutor;
 import com.upmc.pstl2013.alloyGenerator.IAlloyGenerator;
@@ -59,6 +60,8 @@ public class SwtView extends Composite {
 	private final TableEditor editorString, editorBool;
 	private Text txtPersonalPropertie;
 	private Button btnPersonalPropertie;
+	private Text txtTimeOut;
+	private Tree treeExecResult;
 
 	/**
 	 * Create the composite.
@@ -98,7 +101,7 @@ public class SwtView extends Composite {
 		itemAlloyUse = new TabItem(tabFolder, SWT.NONE);
 		itemAlloyUse.setText("Utilisation");
 		cpItemAlloyUse = new Composite(tabFolder, SWT.BORDER);
-		cpItemAlloyUse.setLayout(new GridLayout(2, false));
+		cpItemAlloyUse.setLayout(new GridLayout(3, false));
 		itemAlloyUse.setControl(cpItemAlloyUse);
 		//Items et composite pour la partie propriete du tabeFolder
 		itemAlloyProperty = new TabItem(tabFolder, SWT.NONE);
@@ -153,44 +156,51 @@ public class SwtView extends Composite {
 		txtDirectory.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		txtDirectory.setText(userDir);
 
+		treeExecResult = new Tree(cpItemAlloyUse, SWT.BORDER);
+		treeExecResult.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 5));
+
 		btnChooserFile = new Button(cpItemAlloyUse, SWT.NONE);
 		btnChooserFile.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1));
 		btnChooserFile.setText("Choose File");
+		btnChooserFile.addMouseListener(new EventChooseFile(this));
 
 		txtLogs = new Text(cpItemAlloyUse, SWT.BORDER | SWT.READ_ONLY | SWT.H_SCROLL | SWT.V_SCROLL | SWT.CANCEL | SWT.MULTI);
-		txtLogs.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 5));
+		txtLogs.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 4));
 
 		btnExcuterAlloy = new Button(cpItemAlloyUse, SWT.NONE);
 		btnExcuterAlloy.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1));
 		btnExcuterAlloy.setText("Execute Alloy");
 
+		//Ajout des events
+		btnExcuterAlloy.addMouseListener(new EventCurrentExecutor(this));
+
+		txtTimeOut = new Text(cpItemAlloyUse, SWT.BORDER);
+		txtTimeOut.setText("Time Out");
+		txtTimeOut.setLayoutData(new GridData(SWT.FILL, SWT.TOP, false, false, 1, 1));
+
 		btnReadLogs = new Button(cpItemAlloyUse, SWT.NONE);
-		btnReadLogs.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1));
+		btnReadLogs.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, false, false, 1, 1));
 		btnReadLogs.setText("Read logs");
-		new Label(cpItemAlloyUse, SWT.NONE);
-		new Label(cpItemAlloyUse, SWT.NONE);
+		btnReadLogs.addMouseListener(new EventReadLogs(this));
 
 		btnPersonalPropertie = new Button(cpItemAlloyUse, SWT.NONE);
 		btnPersonalPropertie.setLayoutData(new GridData(SWT.FILL, SWT.TOP, false, false, 1, 1));
 		btnPersonalPropertie.setText("Exec Perso");
+		btnPersonalPropertie.addMouseListener(new EventPersonalExecutor(this));
 
 		txtPersonalPropertie = new Text(cpItemAlloyUse, SWT.BORDER | SWT.V_SCROLL | SWT.MULTI);
-		GridData gd_txtPersonalPropertie = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+		GridData gd_txtPersonalPropertie = new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1);
 		gd_txtPersonalPropertie.heightHint = 65;
 		txtPersonalPropertie.setLayoutData(gd_txtPersonalPropertie);
-
-		//Ajout des events
-		btnExcuterAlloy.addMouseListener(new EventCurrentExecutor(this));
-		btnPersonalPropertie.addMouseListener(new EventPersonalExecutor(this));
 		btnChooseDir.addMouseListener(new EventChooseDir(this));
-		btnReadLogs.addMouseListener(new EventReadLogs(this));
-		btnChooserFile.addMouseListener(new EventChooseFile(this));
 
 		// on finit par une petite vérification...
 		this.checkDirectory(alloyGenerator);
 
 		//Suppression des anciens logs
 		deleteOldLogs();
+		
+		updaTreeExecResult();
 	}
 
 	/**
@@ -222,7 +232,7 @@ public class SwtView extends Composite {
 		for (String prop : AbstractProperties.getProperties()) {
 			TableItem item = new TableItem(tabProperties, SWT.NONE);
 			item.setText(0, prop);
-			
+
 			// si la propriété est dans les préférences, on coche par défaut
 			String prefs = ConfPropertiesManager.getInstance().getProperties();
 			if (prefs.contains(prop)) {
@@ -243,6 +253,22 @@ public class SwtView extends Composite {
 		File logDebug = new File(userDir + "logDebug.html");
 		logInfo.delete();
 		logDebug.delete();
+	}
+	
+	public void updaTreeExecResult()
+	{
+		for (int loopIndex1 = 0; loopIndex1 < 5; loopIndex1++) {
+		      TreeItem item0 = new TreeItem(treeExecResult, 0);
+		      item0.setText("Level 0 Item " + loopIndex1);
+		      for (int loopIndex2 = 0; loopIndex2 < 5; loopIndex2++) {
+		        TreeItem item1 = new TreeItem(item0, 0);
+		        item1.setText("Level 1 Item " + loopIndex2);
+		        for (int loopIndex3 = 0; loopIndex3 < 5; loopIndex3++) {
+		          TreeItem item2 = new TreeItem(item1, 0);
+		          item2.setText("Level 2 Item " + loopIndex3);
+		        }
+		      }
+		    }
 	}
 
 	public Text getTxtDirectory() {
