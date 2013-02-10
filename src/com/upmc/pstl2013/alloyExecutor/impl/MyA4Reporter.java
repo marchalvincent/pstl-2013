4ba00b6240fbcf21705da11ddb95afcc14f57da2
Vincent <Vincent@Vincent-PC>
2013-02-10 14:06:51 +0100

    modif des logs, utilisation du reporter d'alloy pour l'affichage

--- src/com/upmc/pstl2013/alloyExecutor/impl/MyA4Reporter.java
+++ src/com/upmc/pstl2013/alloyExecutor/impl/MyA4Reporter.java
@@ -8,6 +8,12 @@ import edu.mit.csail.sdg.alloy4.A4Reporter;
  */
 public class MyA4Reporter extends A4Reporter {
 
+	private String solver;
+	private int bitwidth;
+	private int maxseq;
+	private int skolemDepth;
+	private int symmetry;
+	
 	private int primaryVars;
 	private int totalVars;
 	private int clauses;
@@ -15,6 +21,12 @@ public class MyA4Reporter extends A4Reporter {
 
 	public MyA4Reporter() {
 		super();
+		solver = "";
+		bitwidth = 0;
+		maxseq = 0;
+		skolemDepth = 0;
+		symmetry = 0;
+		
 		primaryVars = 0;
 		totalVars = 0;
 		clauses = 0;
@@ -44,8 +56,31 @@ public class MyA4Reporter extends A4Reporter {
 	}
 
 	@Override
+	public void translate (String solver, int bitwidth, int maxseq, int skolemDepth, int symmetry) {
+		this.solver = solver;
+		this.bitwidth = bitwidth;
+		this.maxseq = maxseq;
+		this.skolemDepth = skolemDepth;
+		this.symmetry = symmetry;
+		super.translate(solver, bitwidth, maxseq, skolemDepth, symmetry);
+	}
+
+	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
+
+		sb.append("  Solver=");
+		sb.append(solver);
+		sb.append(" Bitwidth=");
+		sb.append(bitwidth);
+		sb.append(" MaxSeq=");
+		sb.append(maxseq);
+		sb.append(" SkolemDepth=");
+		sb.append(skolemDepth);
+		sb.append(" Symmetry=");
+		sb.append(symmetry);
+		sb.append("\n  ");
+		
 		sb.append(totalVars);
 		sb.append(" vars. ");
 		sb.append(primaryVars);
